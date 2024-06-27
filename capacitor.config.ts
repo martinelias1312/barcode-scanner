00ab85e96d8e141e2de6485bd5c8bef7f3f5f715
Martin Eliáš <martin.elias@unite.sk>
2024-06-27 23:12:52 +0200

    master: added texxt recognition

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'barcode-scanner',
+  appId: 'io.test.app',
+  appName: 'test-app',
   webDir: 'www',
   server: {
     androidScheme: 'https'
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { Ocr, TextDetections } from '@capacitor-community/image-to-text';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,49 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  imgSrc: SafeUrl | undefined;
+  lines: string[] = [];
+  message: string = '';
+  busy = false;
+
+  constructor(
+    private sanitizer: DomSanitizer
+  ) {}
+
+  async takePic() {
+    try {
+      this.busy = true;
+      const photo = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera        
+      });
+      const imageUrl = photo.webPath;
+      if (!imageUrl) return;
+      this.imgSrc = this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+      const path = photo.path;
+      if (!path) return;
+      this.lines = await this.processImage(path);
+    } catch (err) {
+      alert(err);
+    } finally {
+      this.busy = false;
+    }
+  }
+
+  async processImage(filename: string): Promise<string[]> {
+    const data: TextDetections = await Ocr.detectText({ filename });
+    // or with orientation -
+    // const textDetections = await td.detectText({ filename, orientation: ImageOrientation.Up })
+
+    const result: string[] = [];
+    for (let detection of data.textDetections) {
+      result.push(detection.text); 
+      //`${detection.text} (${detection.bottomLeft},${detection.bottomRight},${detection.topLeft},${detection.topRight})`);
+    }
+    this.message = result.length === 0 ? 'No text was detected' : '';
+    return result;
+  }
 
 }
